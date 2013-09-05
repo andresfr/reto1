e726aa9384770a02c6bba6fbc054eead96a261a5
andresfr <andresfr@gmail.com>
2013-09-05 16:45:55 -0500

    generación de la Tabla

--- funcionalidades.js
+++ funcionalidades.js
@@ -3,29 +3,28 @@ function generarTabla() {
 	
 	var numerofilas = document.getElementById("filas").value;
 	var numerocolumnas = document.getElementById("columnas").value;
-	var color= document.getElementById("clrColor").value;
-	alert(numerofilas + " " + numerocolumnas+" "+color);
-	var i=0;
-	var j=0;
+	var color = document.getElementById("clrColor").value;
+	alert(parseInt(numerofilas) + " " + parseInt(numerocolumnas)+" "+color);
 	var codigo="<table >";
-	for(i=0;i<numerofilas;i++)   //filas
+	for(var i=0;i<parseInt(numerofilas);i++)   //filas
 	{
-		codigo=codigo + "<tr>";
-		for(j=0;j<numerofilas;j++)   //columnas
+		codigo=codigo + " <tr> ";
+		for(var j=0;j<parseInt(numerocolumnas);j++)   //columnas
 		{
-			if( primo(i+d))
+			if( false)
 			{
-				codigo=codigo + " <td> id= primos " ;
+				codigo=codigo + " <td> id = primos " ;
 			}
 			else
 			{
 				codigo=codigo + " <td> " ;
 			}
-			codigo=codigo + " celda:"+ j +","+ i + "=" + (i+j) + " </td> " ;
+			codigo=codigo + " celda: "+ j +","+ i + "=" + (i+j) + " </td> " ;
 		}
 		codigo=codigo + " </tr>";
 	}
 	codigo = codigo+ "</table>";
-	$("#tabla").append( codigo );
+	alert(numerofilas + " " + numerocolumnas+" "+color + " |||||  "+ codigo);
+	$("#tablaprincipal").append( codigo );
 	//pasar codigo al html
 }
