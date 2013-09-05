17d2b9bde494f9d0695e8195b4cff5c31e908a02
andresfr <andresfr@gmail.com>
2013-09-05 18:43:47 -0500

    Finalizado la entrega

--- funcionalidades.js
+++ funcionalidades.js
@@ -5,7 +5,7 @@ function generarTabla() {
 	var numerocolumnas = document.getElementById("columnas").value;
 	var color = document.getElementById("clrColor").value;
 	alert(parseInt(numerofilas) + " " + parseInt(numerocolumnas)+" "+color);
-	var codigo="<table >";
+	var codigo="<table id=\"tabla1\" border='2'>";
 	var m
 	for(var i=0;i<parseInt(numerofilas);i++)   //filas
 	{
@@ -15,7 +15,8 @@ function generarTabla() {
 			m=i+j;
 			if(m==1||m==2||m==3||m==5||m==7||m==11||m==13||m==17||m==19||m==23||m==29||m==31||m==37||m==41||m==43||m==47||m==53||m==59||m==61||m==67||m==71||m==73||m==79||m==83||m==89||m==97) 		//aqui se evalua si es numero primo
 			{
-				codigo=codigo + " <td> id = primos " ;
+				//codigo=codigo + " <td> id=\"primos\" " ;
+				codigo=codigo + " <td bgcolor=\"" + color + "\"> " ;
 			}
 			else
 			{
@@ -27,7 +28,8 @@ function generarTabla() {
 	}
 	codigo = codigo+ "</table>";
 	alert(numerofilas + " " + numerocolumnas+" "+color + " |||||  "+ codigo);
-	//$("#tablaprincipal").append( codigo );
-	document.write(codigo);
+	document.getElementById("tablaprincipal").innerHTML= codigo;
+	//$("#tablaprincipal").append(codigo);
+	//document.write(codigo);
 	//pasar codigo al html
 }
